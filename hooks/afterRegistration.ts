fd4b27a18133323040c7549d087a5df91e0179d7
Max Malm <benjick@dumfan.net>
2019-06-14 08:27:05 +0200

    Use updated hook signature

--- hooks/afterRegistration.ts
+++ hooks/afterRegistration.ts
@@ -1,5 +1,5 @@
 // This function will be fired both on server and client side context after registering other parts of the module
-export function afterRegistration(Vue, config, store, isServer){
+export function afterRegistration({ Vue, store }){
     let correctPaymentMethod = false
     const placeOrder = function () {
         if (correctPaymentMethod) {
@@ -25,4 +25,3 @@ export function afterRegistration(Vue, config, store, isServer){
         })
     }
 }
-
