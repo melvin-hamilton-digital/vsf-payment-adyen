c5d070fc214c02ee61d7a00860eb3c93ac225f2b
Max Malm <benjick@dumfan.net>
2019-06-14 16:17:56 +0200

    Remove import of deleted file

--- index.ts
+++ index.ts
@@ -1,14 +1,12 @@
 import { createModule, VueStorefrontModuleConfig } from '@vue-storefront/core/lib/module'
 import { module } from './store'
 import { afterRegistration } from './hooks/afterRegistration'
-import { beforeRegistration } from './hooks/beforeRegistration'
 
 export const KEY = 'adyen'
 
 const moduleConfig: VueStorefrontModuleConfig = {
     key: KEY,
     store: { modules: [{ key: KEY, module }] },
-    beforeRegistration,
     afterRegistration
 }
 
